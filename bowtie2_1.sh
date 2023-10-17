338ae9340db7f7bc16ad65787b53a84d4feeb553
B242188-2023 <s2558632@ed.ac.uk>
2023-10-17 11:14:25 +0100

    bowtie2-group1.sh

---
+++ bowtie2_1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#gunzip TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz
+
+#bowtie2-build TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta Index
+
+mapfile -t End1 < "group1-1"
+mapfile -t End2 < "group1-2"
+
+cd /home/s2558632/ICA1/fastqc
+
+for i in "${!End1[@]}"
+do
+	
+	file1="${End1[$i]}"
+	file2="${End2[$i]}"
+	bowtie2 -x /home/s2558632/ICA1/Tcongo_genome/Index -1 "$file1" -2 "$file2" -S /home/s2558632/ICA1/Tcongo_genome/2sets_1.sam -p 1
+done
+
+#bowtie2 -x ./Index -1 {data*_1} -2 {data*_2} -S ./2sets.sam -p 1
